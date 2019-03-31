0d705c36ca5cfe0a309e0d21836081972fa70a58
HaxorHurley <33719181+HaxorHurley@users.noreply.github.com>
2019-03-31 14:38:53 +0200

    Better console logging

--- RusBot/index.js
+++ RusBot/index.js
@@ -11,31 +11,31 @@ client.on('message', message => {
         switch (message.content) {
             case '!hi':
                 message.reply('Hey Im a reply!')
-                .then(sent => console.log('Sent a reply to ${sent.author.username}'))
+                .then(sent => console.log('Sent a reply to ', message.author.username))
                 .catch(console.error);         
                 break;
             
             case '!help':
                 message.reply('Use !hi to say hi, !stream to get a stream link, !ruslingdk for all information or !help for help')
-                .then(sent => console.log('Sent help to ${sent.author.username}'))
+                .then(sent => console.log('Sent help to ', message.author.username))
                 .catch(console.error)
                 break;
 
             case '!stream':
                 message.reply('I see you are looking for the stream team dream team. Go to https://twitch.tv to watch the stream.')
-                .then(sent => console.log('Sent Stream link to ${sent.author.username}'))
+                .then(sent => console.log('Sent Stream link to ', message.author.username))
                 .catch(console.error)
                 break;
 
             case '!ruslingdk':
                 message.reply('You can find all the information needed about "Rusperioden" here: https://www.rusling.dk/.')
-                .then(sent => console.log('Sent ruslingdk link to ${sent.author.username}'))
+                .then(sent => console.log('Sent ruslingdk link to ', message.author.username))
                 .catch(console.error)
                 break;
 
             default:
                 message.reply('Sorry, i do not know what you mean. You are welcome to try again')
-                .then(sent => console.log('Sent default message to ${sent.authoer.username}'))
+                .then(sent => console.log('Sent default message to ', message.author.username))
                 .catch(console.error)
                 break;
         }
