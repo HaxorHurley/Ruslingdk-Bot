e7a6b9f9fd5cd223ad54b25f7eb4f4610c247e01
HaxorHurley <33719181+HaxorHurley@users.noreply.github.com>
2019-03-31 13:54:07 +0200

    added case to message reactions

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ client.on('message', message => {
     if (message.content.charAt(0) === '!'){
         switch (message.content) {
             case '!hi':
-                message.reply('Hey Im a reaply!')
+                message.reply('Hey Im a reply!')
                 .then(sent => console.log('Sent a reply to ${sent.author.username}'))
                 .catch(console.error);         
                 break;
-        
+            
+            case '!help':
+                message.reply('Use !hi to say hi or !help for help')
+                .then(sent => console.log('Sent help to ${sent.author.username}'))
+                .catch(console.error)
             default:
                 break;
         }
