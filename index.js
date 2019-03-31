40757643a6b2ab769f98f40aab4b0f4ab0e3dba9
HaxorHurley <33719181+HaxorHurley@users.noreply.github.com>
2019-03-31 14:13:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,15 +16,27 @@ client.on('message', message => {
                 break;
             
             case '!help':
-                message.reply('Use !hi to say hi, !stream to get a stream link or !help for help')
+                message.reply('Use !hi to say hi, !stream to get a stream link, !ruslingdk for all information or !help for help')
                 .then(sent => console.log('Sent help to ${sent.author.username}'))
                 .catch(console.error)
                 break;
+
             case '!stream':
                 message.reply('I see you are looking for the stream team dream team. Go to https://twitch.tv to watch the stream.')
                 .then(sent => console.log('Sent Stream link to ${sent.author.username}'))
                 .catch(console.error)
+                break;
+
+            case 'ruslingdk':
+                message.reply('You can find all the information needed about "Rusperioden" here: https://www.rusling.dk/.')
+                .then(sent => console.log('Sent ruslingdk link to ${sent.author.username}'))
+                .catch(console.error)
+                break;
+
             default:
+                message.reply('Sorry, i do not know what you mean. You are welcome to try again')
+                .then(sent => console.log('Sent default message to ${sent.authoer.username}'))
+                .catch(console.error)
                 break;
         }
     }
