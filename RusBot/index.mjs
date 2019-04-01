8a0b046c292e8934b7f537d257e30543458fd00f
Bogi <bogi1511@gmail.com>
2019-04-01 20:53:42 +0200

    event now shows more information, updated help command

--- RusBot/index.mjs
+++ RusBot/index.mjs
@@ -23,13 +23,13 @@ client.on('message', (message) => {
         break;
 
       case `${config.prefix}help`:
-        message.reply('Rusbot is a bot made for "Rusperioden" at AAU.\nThe available commands are listed below:\n\t!hi\t\t\t\t\t\t  Say hi to the bot!\n\t!stream\t\t\t\tGet a link to the stream!\n\t!ruslingdk\t\t\tGet a link to the ruslingdk website!\n\t!help\t\t\t\t\t See this message!')
+        message.reply('Rusbot is a bot made for "Rusperioden" at AAU.\nThe available commands are listed below:\n\t!hi\t\t\t\t\t\t  Say hi to the bot!\n\t!lan\t\t\t\tGet a information about RUSLAN!\n\t!ruslingdk\t\t\tGet a link to the ruslingdk website!\n\t!event\t\t\t\t\tSee information about the upcoming events!\n\t!help\t\t\t\t\t See this message!')
           .then(sent => console.log('Sent help to ', message.author.username))
           .catch(console.error);
         break;
 
-      case `${config.prefix}stream`:
-        message.reply('I see you are looking for the stream team dream team. Go to https://twitch.tv to watch the stream.')
+      case `${config.prefix}lan`:
+        message.reply('I see you are looking for the stream team dream team.\n The stream can be found at: https://www.twitch.tv/aauruslan19 \nThe discord server can be found at: {INSERT DISCORD INVITE LINK}')
           .then(sent => console.log('Sent Stream link to ', message.author.username))
           .catch(console.error);
         break;
@@ -48,21 +48,26 @@ client.on('message', (message) => {
         } else {
           api.getEvents()
             .then((response) => {
-              response.json().then((data) => {
-                console.log(data);
+              response.json().then((event) => {
 
                 const today = new Date(2018, 8, 3, 10, 30, 0);
                 let eventFound = false;
-
-                Object.values(data).forEach((event) => {
-                  const beginDate = new Date(event.begin_at);
-                  const endDate = new Date(event.end_at);
+                let nextEvent = ``;
+                Object.keys(event).forEach(function (index) {
+                  const beginDate = new Date(event[index].begin_at);
+                  const endDate = new Date(event[index].end_at);
+                  const nextIndex = (parseInt(index)+1).toString();
                   if (today <= endDate && today.getDate() >= beginDate.getDate() && !eventFound) {
-                    console.log(event);
+                    console.log(event[index]);
+                    if(index < event.length - 1){
+                      nextEvent = `\n\nThe following event is:\n${event[nextIndex].title} @ ${TimeFormatter.formatDateTime(new Date(event[nextIndex].begin_at))}\nYou can find more information about the event here: ${api.getEventUrl()+event[nextIndex].id}`;
+                    }
+
                     message.reply(
-                      `There is an event today at:\n${event.location
-                      } where ${event.title} will be happening. It started at ${TimeFormatter.formatDateTime(beginDate)
-                      }. You can find more information about the event here: ${api.getEventUrl()+event.id}`,
+                      `There is an event today at:\n${event[index].location
+                      } where ${event[index].title} will be happening. It started at ${TimeFormatter.formatDateTime(beginDate)
+                      }. You can find more information about the event here: ${api.getEventUrl()+event[index].id
+                      }${nextEvent}`,
                     )
                       .then(sent => console.log('Sent events to ', message.author.username))
                       .catch(console.error);
@@ -70,9 +75,9 @@ client.on('message', (message) => {
                     return;
                   }
 
-                  if (today <= beginDate && !eventFound) {
-                    console.log(event);
-                    message.reply(`The next event is:\n${event.title} @ ${TimeFormatter.formatDateTime(beginDate)}\nYou can find more information about the event here: ${api.getEventUrl()+event.id}`)
+                  if (today.getDate() < beginDate.getDate() && !eventFound) {
+                    console.log(event[index]);
+                    message.reply(`The next event is:\n${event[index].title} @ ${TimeFormatter.formatDateTime(beginDate)}\nYou can find more information about the event here: ${api.getEventUrl()+event[index].id}`)
                       .then(sent => console.log('Sent events to ', message.author.username))
                       .catch(console.error);
                     eventFound = true;
@@ -105,7 +110,11 @@ client.on('message', (message) => {
         
         /* eslint-disable-next-line prefer-destructuring */
         break;
-
+      case `${config.prefix}yeet`:
+        message.reply(`SUBSCRIBE TO PEWDIEPIE!\nHVIS DU FINDER MEYER, SKYLDER HAN AT DANSE ORANGE JUSTICE!`)
+          .then(sent => console.log('Sent ruslingdk link to ', message.author.username))
+          .catch(console.error);
+        break;
       default:
         break;
     }
