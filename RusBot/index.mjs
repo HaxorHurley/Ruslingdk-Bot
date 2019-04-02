4ec9eb464883dc292d055ec21f7044d23dd8fa88
Bogi <bogi1511@gmail.com>
2019-04-02 09:27:21 +0200

    Fixed permissions to set host. Changed replies to be embedded.

--- RusBot/index.mjs
+++ RusBot/index.mjs
@@ -17,32 +17,109 @@ client.on('message', (message) => {
       // Todo: Refactor this with first-class.citizens; of a commands objects:
       // e.g. { "command": () => { ... } }
       case `${config.prefix}hi`:
-        message.reply('Hey Im a reply!')
+        message.reply('Hello, I am Ruslingdk-Bot!')
           .then(sent => console.log('Sent a reply to ', message.author.username))
           .catch(console.error);
         break;
 
       case `${config.prefix}help`:
-        message.reply('Rusbot is a bot made for "Rusperioden" at AAU.\nThe available commands are listed below:\n\t!hi\t\t\t\t\t\t  Say hi to the bot!\n\t!lan\t\t\t\tGet a information about RUSLAN!\n\t!ruslingdk\t\t\tGet a link to the ruslingdk website!\n\t!event\t\t\t\t\tSee information about the upcoming events!\n\t!help\t\t\t\t\t See this message!')
-          .then(sent => console.log('Sent help to ', message.author.username))
+        message.reply({embed: {
+          color: 3447003,
+          author: {
+            name: client.user.username,
+            icon_url: client.user.avatarURL
+          },
+          title: "Help",
+          description: 'Rusbot is a bot made for "Rusperioden" at AAU.\nThe available commands are listed below.',
+          fields: [{
+              name: "!hi",
+              value: "Say hi to the bot!"
+            },
+            {
+              name: "!lan",
+              value: "Get a information about RUSLAN!"
+            },
+            {
+              name: "!ruslingdk",
+              value: `Get a link to the ruslingdk website!`
+            },
+            {
+              name: "!event",
+              value: `get information about the current or next event!`
+            },
+            {
+              name: "!help",
+              value: `See this message!`
+            }
+          ],
+          timestamp: new Date()
+          }
+        })
+        
+          .then(sent => console.log('Sent help information to ', message.author.username))
           .catch(console.error);
         break;
 
       case `${config.prefix}lan`:
-        message.reply('I see you are looking for the stream team dream team.\n The stream can be found at: https://www.twitch.tv/aauruslan19 \nThe discord server can be found at: {INSERT DISCORD INVITE LINK}')
-          .then(sent => console.log('Sent Stream link to ', message.author.username))
+        message.reply({embed: {
+          color: 3447003,
+          author: {
+            name: client.user.username,
+            icon_url: client.user.avatarURL
+          },
+          title: "Ruslan",
+          description: 'I see you are looking for the stream team dream team.',
+          fields: [{
+              name: "Links",
+              value: "[RUSLAN Twitch Stream](https://www.twitch.tv/aauruslan19)\n[RUSLAN Discord Server](INSERT LINK HERE)"
+            },
+          ],
+          timestamp: new Date()
+          }
+        })
+          .then(sent => console.log('Sent lan information to ', message.author.username))
           .catch(console.error);
         break;
 
       case `${config.prefix}ruslingdk`:
-        message.reply(`You can find all the information needed about "Rusperioden" here: ${api.getBaseUrl()}.`)
+        message.reply({embed: {
+          color: 3447003,
+          author: {
+            name: client.user.username,
+            icon_url: client.user.avatarURL
+          },
+          title: "Rusling.dk",
+          description: 'You can find all the information needed about "Rusperioden" here.',
+          fields: [{
+              name: "Link",
+              value: `[rusling.dk](${api.getBaseUrl()})`
+            },
+          ],
+          timestamp: new Date()
+          }
+        })
           .then(sent => console.log('Sent ruslingdk link to ', message.author.username))
           .catch(console.error);
         break;
 
       case `${config.prefix}event`:
         if (typeof hosts[message.guild.id] === 'undefined') {
-          message.reply(`No host set. Use "${config.prefix}sethost hostname" to set host.`)
+          message.reply({embed: {
+            color: 3447003,
+            author: {
+              name: client.user.username,
+              icon_url: client.user.avatarURL
+            },
+            title: "Event",
+            description: 'You can find all the information needed about "Rusperioden" here.',
+            fields: [{
+                name: "No host set.",
+                value: `Use "${config.prefix}sethost hostname" to set host.\nOnly admins can do this.`
+              },
+            ],
+            timestamp: new Date()
+            }
+          })
             .then(sent => console.log('Host missing. Sent how to change host information to ', message.author.username))
             .catch(console.error);
         } else {
@@ -52,7 +129,7 @@ client.on('message', (message) => {
 
                 const today = new Date(2018, 8, 3, 10, 30, 0);
                 let eventFound = false;
-                let nextEvent = ``;
+                let nextEvent = `There is no next event.`;
                 Object.keys(event).forEach(function (index) {
                   const beginDate = new Date(event[index].begin_at);
                   const endDate = new Date(event[index].end_at);
@@ -60,16 +137,39 @@ client.on('message', (message) => {
                   if (today <= endDate && today.getDate() >= beginDate.getDate() && !eventFound) {
                     console.log(event[index]);
                     if(index < event.length - 1){
-                      nextEvent = `\n\nThe following event is:\n${event[nextIndex].title} @ ${TimeFormatter.formatDateTime(new Date(event[nextIndex].begin_at))}\nYou can find more information about the event here: ${api.getEventUrl()+event[nextIndex].id}`;
+                      nextEvent = `${event[nextIndex].title} @ ${TimeFormatter.formatDateTime(new Date(event[nextIndex].begin_at))}\nYou can find more information about the event here: ${api.getEventUrl()+event[nextIndex].id}`;
                     }
 
-                    message.reply(
-                      `There is an event today at:\n${event[index].location
-                      } where ${event[index].title} will be happening. It started at ${TimeFormatter.formatDateTime(beginDate)
-                      }. You can find more information about the event here: ${api.getEventUrl()+event[index].id
-                      }${nextEvent}`,
-                    )
-                      .then(sent => console.log('Sent events to ', message.author.username))
+                    message.reply({embed: {
+                      color: 3447003,
+                      author: {
+                        name: client.user.username,
+                        icon_url: client.user.avatarURL
+                      },
+                      title: "Event",
+                      description: 'There is an event today!',
+                      fields: [
+                        {
+                          name: `${event[index].title}`,
+                          value: `${event[index].description}`
+                        },
+                        {
+                          name: "Time",
+                          value: `${TimeFormatter.formatDateTime(beginDate)}`
+                        },
+                        {
+                          name: "Place",
+                          value: `${event[index].location}`
+                        },
+                        {
+                          name: "Next Event",
+                          value: `${nextEvent}`
+                        }
+                      ],
+                      timestamp: new Date()
+                      }
+                    })
+                      .then(sent => console.log('Sent current and next event to ', message.author.username))
                       .catch(console.error);
                     eventFound = true;
                     return;
@@ -77,8 +177,32 @@ client.on('message', (message) => {
 
                   if (today.getDate() < beginDate.getDate() && !eventFound) {
                     console.log(event[index]);
-                    message.reply(`The next event is:\n${event[index].title} @ ${TimeFormatter.formatDateTime(beginDate)}\nYou can find more information about the event here: ${api.getEventUrl()+event[index].id}`)
-                      .then(sent => console.log('Sent events to ', message.author.username))
+                    message.reply({embed: {
+                      color: 3447003,
+                      author: {
+                        name: client.user.username,
+                        icon_url: client.user.avatarURL
+                      },
+                      title: "Event",
+                      description: 'The next event is!',
+                      fields: [
+                        {
+                          name: `${event[index].title}`,
+                          value: `${event[index].description}`
+                        },
+                        {
+                          name: "Time",
+                          value: `${TimeFormatter.formatDateTime(beginDate)}`
+                        },
+                        {
+                          name: "Place",
+                          value: `${event[index].location}`
+                        },
+                      ],
+                      timestamp: new Date()
+                      }
+                    })
+                      .then(sent => console.log('Sent next event to ', message.author.username))
                       .catch(console.error);
                     eventFound = true;
                     return;
@@ -86,8 +210,18 @@ client.on('message', (message) => {
                 });
 
                 if (!eventFound) {
-                  message.reply('There are no more events! Become a tutor next year, and take part in the fun again!')
-                    .then(sent => console.log('Sent events to ', message.author.username))
+                  message.reply({embed: {
+                    color: 3447003,
+                    author: {
+                      name: client.user.username,
+                      icon_url: client.user.avatarURL
+                    },
+                    title: "Event",
+                    description: 'There are no more events!',
+                    timestamp: new Date()
+                    }
+                  })
+                    .then(sent => console.log('No events found. Sent reply to ', message.author.username))
                     .catch(console.error);
                 }
               });
@@ -97,23 +231,53 @@ client.on('message', (message) => {
 
       case `${config.prefix}sethost`:
         let host = message.content.split(' ')[1];
-        if(typeof host === 'undefined') {
-          message.reply(`You must specify a host`)
-          .then(sent => console.log(`Host not specified. Nothing changed.\nChanged by: ${message.author.username}`))
-          .catch(console.error);
+        if(message.member.hasPermission('ADMINISTRATOR')){
+          if(typeof host === 'undefined') {
+            message.reply({embed: {
+              color: 3447003,
+              author: {
+                name: client.user.username,
+                icon_url: client.user.avatarURL
+              },
+              title: "Set Host",
+              description: 'You must specify a host',
+              timestamp: new Date()
+              }
+            })
+            .then(sent => console.log(`Host not specified. Nothing changed.\nChanged by: ${message.author.username}`))
+            .catch(console.error);
+          } else {
+            hosts[message.guild.id] = host;
+            message.reply({embed: {
+              color: 3447003,
+              author: {
+                name: client.user.username,
+                icon_url: client.user.avatarURL
+              },
+              title: "Set Host",
+              description: `Host set to: ${host}`,
+              timestamp: new Date()
+              }
+            })
+              .then(sent => console.log(`Host changed to: ${host}\nChanged by: ${message.author.username}`))
+              .catch(console.error);
+          }
         } else {
-          hosts[message.guild.id] = host;
-          message.reply(`Host set to: ${host}`)
-            .then(sent => console.log(`Host changed to: ${host}\nChanged by: ${message.author.username}`))
+          message.reply({embed: {
+            color: 3447003,
+            author: {
+              name: client.user.username,
+              icon_url: client.user.avatarURL
+            },
+            title: "Set Host",
+            description: `Only users with admin permission can set host.`,
+            timestamp: new Date()
+            }
+          })
+            .then(sent => console.log(`Non admin user tried to change host.\nUser: ${message.author.username}`))
             .catch(console.error);
         }
-        
         /* eslint-disable-next-line prefer-destructuring */
-        break;
-      case `${config.prefix}yeet`:
-        message.reply(`SUBSCRIBE TO PEWDIEPIE!\nHVIS DU FINDER MEYER, SKYLDER HAN AT DANSE ORANGE JUSTICE!`)
-          .then(sent => console.log('Sent ruslingdk link to ', message.author.username))
-          .catch(console.error);
         break;
       default:
         break;
@@ -122,3 +286,5 @@ client.on('message', (message) => {
 });
 console.log("Logging in")
 client.login(config.token);
+
+
